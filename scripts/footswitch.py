4d651e641c8287c1bd2a014ec8185b0d5eec15ad
Toby Buckley <buckley.toby@gmail.com>
2023-11-07 16:21:54 -0600

    made publish topic relative

--- scripts/footswitch.py
+++ scripts/footswitch.py
@@ -39,7 +39,7 @@ class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('simple_footswitch')
-        self.publisher_ = self.create_publisher(Bool, '/footswitch/triggered', 10)
+        self.publisher_ = self.create_publisher(Bool, 'footswitch/triggered', 10)
         timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
