a033c1f2cde4fdc4707a2c4d3bd8502d93f5729d
nu-haptics <nu.haptics@gmail.com>
2023-11-28 15:55:46 -0600

    works with path instead of vendor and product id

--- scripts/footswitch.py
+++ scripts/footswitch.py
@@ -11,28 +11,11 @@ from rclpy.node import Node
 from std_msgs.msg import Bool
 
 SHUTDOWN = False
-STATE = False
-VENDOR_ID = 1523 # VEC infinity pedal
-PRODUCT_ID = 255 # VEC infinity pe
+# VENDOR_ID = 1523 # VEC infinity pedal
+# PRODUCT_ID = 255 # VEC infinity pedal
+# DEV_PATH = b'/dev/hidraw0' # default for one of the infinity pedals
 
-def update_hid():
-    global STATE
-    while True:
-        # out = hid.Device(VENDOR_ID, PRODUCT_ID).read(64) # string, should be something like b'\x00\x00'
-        try:
-            out = int.from_bytes(hid.Device(VENDOR_ID, PRODUCT_ID).read(64), byteorder='little') # https://www.tutorialspoint.com/how-to-convert-bytes-to-int-in-python
-        except:
-            out = 0
-        if out != 0:
-            STATE = True
-        else:
-            STATE = False
 
-        # shutdown
-        if SHUTDOWN:
-            break
-        print(out)
-        time.sleep(0.01) # go fast so that events aren't missed
 
 
 
@@ -43,11 +26,33 @@ class MinimalPublisher(Node):
         self.publisher_ = self.create_publisher(Bool, 'footswitch/triggered', 10)
         timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.declare_parameter('dev_path', '/dev/hidraw0')
+        dev_path = self.get_parameter('dev_path').get_parameter_value().string_value
+        self.dev_path = str.encode(dev_path)
+        self.state = False
 
     def timer_callback(self):
         msg = Bool()
-        msg.data = STATE
+        msg.data = self.state
         self.publisher_.publish(msg)
+
+    def update_hid(self):
+        while True:
+            # out = hid.Device(VENDOR_ID, PRODUCT_ID).read(64) # string, should be something like b'\x00\x00'
+            try:
+                out = int.from_bytes(hid.Device(path=self.dev_path).read(64), byteorder='little') # https://www.tutorialspoint.com/how-to-convert-bytes-to-int-in-python
+            except:
+                out = 0
+            if out != 0:
+                self.state = True
+            else:
+                self.state = False
+
+            # shutdown
+            if SHUTDOWN:
+                break
+            print(out)
+            time.sleep(0.01) # go fast so that events aren't missed
 
 
 def main(args=None):
@@ -57,7 +62,7 @@ def main(args=None):
         minimal_publisher = MinimalPublisher()
 
         # make the hid thread
-        x = threading.Thread(target=update_hid)
+        x = threading.Thread(target=minimal_publisher.update_hid)
         x.daemon = True
         x.start()
 
